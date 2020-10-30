e47cf63fbc38ccb80211a82197a858de71c8756e
Silvio Rodrigues Vieira <srodrvie@everis.com>
2020-10-30 17:09:38 -0300

    3 commit

---
+++ BancoDeDados/VIEWS.sql
@@ -0,0 +1,55 @@
+
+CREATE VIEW Nome_livro
+AS
+SELECT 
+*
+FROM
+Livros
+
+SELECT*FROM Nome_livros
+CREATE VIEW Dados_Alunos
+AS
+SELECT 
+*
+FROM
+    Alunos
+
+
+SELECT *FROM Dados_Alunos
+
+
+
+
+CREATE VIEW Visualizar_Requisicoes
+AS
+SELECT 
+*
+FROM
+    Requisicoes
+
+
+SELECT *FROM Visualizar_Requisicoes
+
+
+CREATE VIEW Visualizar_Categorias
+AS
+
+SELECT *FROM
+    Categorias
+
+SELECT *FROM Visualizar_Categorias
+
+
+
+
+CREATE VIEW Visualizar_Autores
+AS
+
+SELECT *FROM
+
+
+SELECT *FROM Visualizar_Autores
+
+
+
+
